8505d987717899e1b67a2749af4d27dcc475e464
Selmaa <144896543+SelmaaSah@users.noreply.github.com>
2023-12-01 11:49:11 +0100

    v 0.6 Selma

--- src/V_connexion.java
+++ src/V_connexion.java
@@ -1,6 +1,9 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import javax.swing.*;
 
@@ -13,6 +16,8 @@ public class V_connexion {
 
     private JLabel mdpLabel;
     private JTextField mdpTextField;
+    
+    private JLabel bienvenueLabel;
 
     private JButton buttonConnexion;
 
@@ -20,7 +25,7 @@ public class V_connexion {
     
     public V_connexion() {
         connexionPanel = new JPanel(new GridBagLayout()); // Utilisation de GridBagLayout
-        GridBagConstraints gbc = new GridBagConstraints();
+
 
 
         
@@ -29,39 +34,84 @@ public class V_connexion {
         identifiantTextField.setPreferredSize(new Dimension(120, 20));
 
         buttonConnexion = new JButton("Se Connecter");
-
+        
+        bienvenueLabel = new JLabel("Bienvenue, GSB est ravi de vous revoir !");
         mdpLabel = new JLabel("Mot de Passe :  ");
         mdpTextField = new JTextField("");
         mdpTextField.setPreferredSize(new Dimension(120, 20));
 
 
+		Font police = new Font("Courier New", Font.BOLD, 13);
+		bienvenueLabel.setFont(police);
+		
+		
+        // Chargement de l'image depuis le fichier
+//        ImageIcon logoSIO = new ImageIcon("image/img-sioplus.png"); 
+//
+//        // Redimensionner l'image
+//        Image image = logoSIO.getImage();
+//        Image newImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH); 
+//
+//        ImageIcon resizedIcon = new ImageIcon(newImage);
+//        imageLabel = new JLabel(resizedIcon);
+//        mainPanel.add(imageLabel); // Ajout du JLabel avec l'image en dessous du bouton
         
         // Configuration pour le JLabel et le JTextField de l'identifiant
 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.WEST;
+        
+        
+     // Pour bienvenueLabel
+        GridBagConstraints gbcBienvenue = new GridBagConstraints();
+        gbcBienvenue.gridx = 0;
+        gbcBienvenue.gridy = 0;
+        gbcBienvenue.anchor = GridBagConstraints.WEST;
+        gbcBienvenue.gridwidth = 2;
+        gbcBienvenue.insets = new Insets(10, 0, 40, 0); // Ajout d'un espace en HAUT ET EN bas
 
-        gbc.insets = new Insets(0, 0, 10, 0); // Ajout d'un espace en HAUT ET EN bas
-        connexionPanel.add(identifiantLabel, gbc);
+        connexionPanel.add(bienvenueLabel, gbcBienvenue);
 
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.insets = new Insets(0, 0, 10, 0); // Ajout d'un espace en bas
-        connexionPanel.add(identifiantTextField, gbc);
+        // Pour identifiantLabel
+        GridBagConstraints gbcIdentifiantLabel = new GridBagConstraints();
+        gbcIdentifiantLabel.gridx = 0;
+        gbcIdentifiantLabel.gridy = 1;
+        gbcIdentifiantLabel.insets = new Insets(10, 0, 10, 0); // Ajout d'un espace en HAUT ET EN bas
+        connexionPanel.add(identifiantLabel, gbcIdentifiantLabel);
 
-        // Configuration pour le JLabel et le JTextField du mot de passe
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(10, 0, 10, 0); // Ajout d'un espace en haut
-        connexionPanel.add(mdpLabel, gbc);
+        // Pour identifiantTextField
+        GridBagConstraints gbcIdentifiantTextField = new GridBagConstraints();
+        gbcIdentifiantTextField.gridx = 1;
+        gbcIdentifiantTextField.gridy = 1;
+        gbcIdentifiantTextField.insets = new Insets(10, 0, 10, 0); // Ajout d'un espace en bas
+        connexionPanel.add(identifiantTextField, gbcIdentifiantTextField);
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(10, 0, 0, 0); // Ajout d'un espace en haut
-        connexionPanel.add(mdpTextField, gbc);
 
-        // Configuration pour le JLabel de la connexion et le bouton
+        GridBagConstraints gbcMdpLabel = new GridBagConstraints();
+        gbcMdpLabel.gridx = 0;
+        gbcMdpLabel.gridy = 2;
+        gbcMdpLabel.insets = new Insets(10, 0, 10, 0); // Ajout d'un espace en HAUT ET EN bas
+        connexionPanel.add(mdpLabel, gbcMdpLabel);
+        
+        
+        GridBagConstraints gbcMdpTextField = new GridBagConstraints();
+        gbcMdpTextField.gridx = 1;
+        gbcMdpTextField.gridy = 2;
+        gbcMdpTextField.insets = new Insets(10, 0, 10, 0); // Ajout d'un espace en HAUT ET EN bas
+        connexionPanel.add(mdpTextField, gbcMdpTextField);
+        
+//        // Configuration pour le JLabel et le JTextField du mot de passe
+//        gbc.gridx = 0;
+//        gbc.gridy = 1;
+//        gbc.insets = new Insets(10, 0, 10, 0); // Ajout d'un espace en haut
+//        connexionPanel.add(mdpLabel, gbc);
+//
+//        gbc.gridx = 1;
+//        gbc.gridy = 1;
+//        gbc.insets = new Insets(10, 0, 0, 0); // Ajout d'un espace en haut
+//        connexionPanel.add(mdpTextField, gbc);
+//
+//        // Configuration pour le JLabel de la connexion et le bouton
+        GridBagConstraints gbc = new GridBagConstraints();
+
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
@@ -72,6 +122,8 @@ public class V_connexion {
         gbc.gridy = 3;
         gbc.gridwidth = 2;
         connexionPanel.add(buttonConnexion, gbc);
+        
+        
     }
 
     public JPanel getConnexionPanel() {
--- src/V_principale.java
+++ src/V_principale.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JFrame;
@@ -7,6 +8,8 @@ import javax.swing.JPanel;
 public class V_principale extends JFrame{
 	
     private JPanel mainPanel;
+    private JPanel monPanelH;
+
     private JLabel connexionBDD;
     private JLabel bienvenue;
 
@@ -20,7 +23,9 @@ public class V_principale extends JFrame{
         
         // Instanciation des �l�ments
         mainPanel = new JPanel();
-        bienvenue = new JLabel("Bienvenue, GSB est ravi de vous revoir !");
+
+        //mainPanel.setLayout(new BorderLayout());
+
 
         
        
@@ -29,12 +34,21 @@ public class V_principale extends JFrame{
         V_connexion connexionPanel = new V_connexion();
        
         // Changement de Couleur du Fond
-        mainPanel.setBackground(Color.ORANGE);
+        
+        int redValue = 128;     // Valeur du composant rouge (entre 0 et 255)
+        int greenValue = 162;   // Valeur du composant vert (entre 0 et 255)
+        int blueValue = 205;  // Valeur du composant bleu (entre 0 et 255)
+
+        Color customColor = new Color(redValue, greenValue, blueValue);
+        
+        mainPanel.setBackground(customColor);
         
         // Ajout du JLabel � notre JPanel
         //connexionPanelmainPanel.add(connexionBDD);
 
-        mainPanel.add(bienvenue);
+        
+
+
         // Ajout du JPanel � la fen�tre principale
         
         getContentPane().add(mainPanel);
