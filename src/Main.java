963f7aa7c03a7255f84fb678be886ef7d62b9e38
Selmaa <144896543+SelmaaSah@users.noreply.github.com>
2023-12-08 11:12:16 +0100

    V 0.18 Selma

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Controleur c = new Controleur();
-<<<<<<< HEAD
-		
-=======
->>>>>>> branch 'master' of https://github.com/SelmaaSah/GSB2023.git
+
 	}
 }
