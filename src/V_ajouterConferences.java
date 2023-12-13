6cacae7017446729faebccb86ffc68acd4a5b8eb
karim-dev-2023 <mr.karimayub@gmail.com>
2023-12-13 11:21:03 +0100

    V 0.26 Karim

--- src/V_ajouterConferences.java
+++ src/V_ajouterConferences.java
@@ -1,51 +1,59 @@
 import java.awt.*;
-
 import javax.swing.*;
 
 public class V_ajouterConferences {
-	private JPanel ajouterConferencePanel;
-	private JLabel titreLabel;
-	
-	private JLabel themeLabel;
-	private JTextField themeTextField;
-	
-	private JLabel dateDeroulementLabel;
-	private JTextField dateDeroulementTextField;
-	
-	public V_ajouterConferences() {
-		ajouterConferencePanel = new JPanel();
-		ajouterConferencePanel.setPreferredSize(new Dimension (500,500));
-		
-		themeLabel = new JLabel("Le thème de la conférence : ");
-		themeTextField = new JTextField();
-		themeTextField.setPreferredSize(new Dimension (150,20));
-		
-		dateDeroulementLabel = new JLabel("La date du conférence :");
-		dateDeroulementTextField = new JTextField();
-		dateDeroulementTextField.setPreferredSize(new Dimension (150,20));
-		
-		
-		titreLabel = new JLabel("Gerer les Conférences");
-		
-		ajouterConferencePanel.setLayout(new GridBagLayout());
+    private JPanel ajouterConferencePanel;
+    private JLabel titreLabel;
+    private JLabel themeLabel;
+    private JTextField themeTextField;
+    private JLabel dateDeroulementLabel;
+    private JTextField dateDeroulementTextField;
+    private JComboBox<String> votreComboBox; // Remplacez String par le type d'éléments que vous voulez dans votre JComboBox
+
+    public V_ajouterConferences() {
+        ajouterConferencePanel = new JPanel();
+        ajouterConferencePanel.setPreferredSize(new Dimension(500, 500));
+
+        themeLabel = new JLabel("Le thème de la conférence : ");
+        themeTextField = new JTextField();
+        themeTextField.setPreferredSize(new Dimension(150, 20));
+
+        dateDeroulementLabel = new JLabel("La date du conférence :");
+        dateDeroulementTextField = new JTextField();
+        dateDeroulementTextField.setPreferredSize(new Dimension(150, 20));
+
+        votreComboBox = new JComboBox<>(); // Vous pouvez ajouter des éléments à votre JComboBox ici
+
+        titreLabel = new JLabel("Gerer les Conférences");
+        
+        votreComboBox = new JComboBox<>();
+        
+//      Faire une For pour recuperer le nom de l'animateur Requete SQL
+        votreComboBox.addItem("chien");
+        votreComboBox.addItem("chat");
+        votreComboBox.addItem("lyon");
+
+        ajouterConferencePanel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.WEST;
-        gbc.insets = new Insets(10, 0, 0, 0); 
-		
-		ajouterConferencePanel.add(titreLabel,gbc);
-		gbc.gridy++;
-		ajouterConferencePanel.add(themeLabel,gbc);
-		gbc.gridy++;
-		ajouterConferencePanel.add(themeTextField,gbc);
-		gbc.gridy++;
-		ajouterConferencePanel.add(dateDeroulementLabel,gbc);
-		gbc.gridy++;
-		ajouterConferencePanel.add(dateDeroulementTextField,gbc);
-	}
-	
-	public JPanel getAjouterConferencePanel() {
-		return ajouterConferencePanel;
-	}
+        gbc.insets = new Insets(10, 0, 0, 0);
+
+        ajouterConferencePanel.add(titreLabel, gbc);
+        gbc.gridy++;
+        ajouterConferencePanel.add(themeLabel, gbc);
+        gbc.gridy++;
+        ajouterConferencePanel.add(themeTextField, gbc);
+        gbc.gridy++;
+        ajouterConferencePanel.add(dateDeroulementLabel, gbc);
+        gbc.gridy++;
+        ajouterConferencePanel.add(dateDeroulementTextField, gbc);
+        gbc.gridy++;
+        ajouterConferencePanel.add(votreComboBox, gbc);
+    }
+
+    public JPanel getAjouterConferencePanel() {
+        return ajouterConferencePanel;
+    }
 }
