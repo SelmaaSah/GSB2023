f90e3505ce4c8c14c1eec129025a71735f5a87be
Selmaa <144896543+SelmaaSah@users.noreply.github.com>
2023-12-20 10:37:52 +0100

    v 0.65 selma

--- Test Unitaire/Test.java
+++ Test Unitaire/Test.java
@@ -1,5 +1,7 @@
 
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,12 +25,53 @@ class Test {
 //		Assert.assertNotNull("L'utilisateur n'existe pas",Modele.existeUser("kayoub", "123"));
 //	}
 	
-	void testGetLesConferences() {
-		Assert.assertNotNull("La fonction ne foncitonne pas", Modele.getLesConferences());
+//	void testGetLesConferences() {
+//		Assert.assertNotNull("La fonction ne fonctionne pas", Modele.getLesConferences());
+//	}
+//	
+//	void testGetLesAnimateur() {
+//		Assert.assertNotNull("La fonction ne fonctionne pas", Modele.getLesAnimateur());
+//	}
+	
+//    void testInsererNvConference() {
+//        // Données de test
+//        String theme = "Modifier pour ecrire ce que l'on veut";
+//        String dateDeroulement = "2023-12-20";
+//        int idAnimateur = 1;
+//
+//        // Appel de la fonction 
+//        Modele.insererNvConference(theme, dateDeroulement, idAnimateur);
+//	}
+	
+//    void testinsertNvUtilisateur() {
+//        // Données de test
+//        String nom = "Rolland";
+//        String prenom = "G";
+//        String login = "Grolland";
+//        String mdp = "123";
+//        String typeVisiteur = "Animateur";
+//
+//
+//        // Appel de la fonction
+//        Modele.insertNvUtilisateur(nom, prenom, login, mdp, typeVisiteur);
+//	}
+	
+	void testSupprimerUtilisateur() {
+	    // Données de test
+	    int idUserASupprimer = 6; // id d'un user
+
+	    // Appel de la fonction à tester
+	    boolean resultat = Modele.getSupprimerUnUtilisateur(idUserASupprimer);
+
+	    // Verification de la reussite de la suppression
+	    Assert.assertTrue(resultat);
 	}
 	
-	void testGetLesAnimateur() {
-		Assert.assertNotNull("La fonction ne foncitonne pas", Modele.getLesAnimateur());
-	}
+
+	
+
+	
+	
+	
 
 }
